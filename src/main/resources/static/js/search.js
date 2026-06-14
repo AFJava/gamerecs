cb53df95b0615e5e14f5e961e43bb11228c8b71a
AFJava <ayfeng2006@gmail.com>
2026-06-14 19:27:26 -0400

    Adjusted search result and rating interface formatting, fixed searches appending to previous searces, formatted rating interface

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,5 +1,5 @@
 const searchbar = document.querySelector(".search input");
-const resultsDiv = document.querySelector(".search_results")
+const resultsDiv = document.querySelector(".search-results")
 
 let debounceTimeout;
 
@@ -14,22 +14,27 @@ searchbar.addEventListener("input", () => {
 
 //Check whether a button was clicked within the search results div
 resultsDiv.addEventListener("click", (event) => {
-    if(event.target.classList.contains("game_rate")) {
+    if(event.target.classList.contains("rate-button")) {
         console.log("Button clicked");
 
         const rateButton = event.target;
         const gameName = rateButton.dataset.gameName;
         const gameId = rateButton.dataset.gameId;
 
+        //Build interface div
         const rateInterface = document.createElement("div");
 
         console.log("div created");
 
         rateInterface.classList.add("rate");
-        rateInterface.innerHTML = `<p>Rate ${gameName} and add it to your profile:</p>`;
+        rateInterface.innerHTML = `<p>Rate ${gameName} and add it to your profile:</p>
+        <form method="post" class="rate-form">
+            <span><input type="number" name="rating" min="1" max="10"> / 10</span>
+            <button type = "submit">Submit rating and add to profile</button>
+        </form>`;
 
         //Get gameDiv by gameId and append rating div
-        const gameDiv = event.target.closest(".search_item");
+        const gameDiv = event.target.closest(".search-item");
 
         //Check if there is an existing rating interface
         const current = document.querySelector(".rate");
@@ -48,6 +53,9 @@ resultsDiv.addEventListener("click", (event) => {
 async function search() {
     const searchContent = searchbar.value;
 
+    //Clear search results
+    resultsDiv.replaceChildren();
+
     //DO NOT search if under 3 alphanumeric characters are given
     if(searchContent.trim().length < 3) {
         return;
@@ -63,12 +71,12 @@ async function search() {
 
     games.results.forEach(game => {
         const gameDiv = document.createElement("div");
-        gameDiv.classList.add("search_item");
+        gameDiv.classList.add("search-item");
         gameDiv.dataset.gameId = `${game.id}`; //Same id used for button
 
-        gameDiv.innerHTML = `<img src = ${game.background_image} class = "game_preview">
-            <p class = "game_name">${game.name}</p>
-            <button class = "game_rate" data-game-id = "${game.id}" data-game-name = "${game.name}">Rate and add to profile</button>`;
+        gameDiv.innerHTML = `<img src = ${game.background_image} class = "game-preview">
+            <h2 class = "game-name">${game.name}</h2>
+            <button type="submit" class = "rate-button" data-game-id = "${game.id}" data-game-name = "${game.name}">Rate and add to profile</button>`;
         
         resultsDiv.appendChild(gameDiv);
     });
