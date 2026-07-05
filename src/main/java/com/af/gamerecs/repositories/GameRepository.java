80b622d32a0a3a1665d1f6f473af8c74ee468b68
AFJava <ayfeng2006@gmail.com>
2026-07-04 23:58:46 -0400

    Force game retrieval to also retrieve collections

--- src/main/java/com/af/gamerecs/repositories/GameRepository.java
+++ src/main/java/com/af/gamerecs/repositories/GameRepository.java
@@ -3,11 +3,22 @@ package com.af.gamerecs.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.af.gamerecs.entities.Game;
 
 public interface GameRepository extends JpaRepository<Game, Integer> {
     Optional<Game> findByIgdbId(Long igdbId);
+
+    @EntityGraph(attributePaths = {
+        "franchises",
+        "genres",
+        "themes",
+        "gameModes",
+        "playerPerspectives",
+        "platforms",
+        "keywords"
+    })
     List<Game> findAllByIgdbIdIn(List<Long> igdbIds);
 }
