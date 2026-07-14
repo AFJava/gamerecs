83a9700fa083f2fc6bff713e979ffe010b99a716
AFJava <ayfeng2006@gmail.com>
2026-07-13 23:27:37 -0400

    Fix inclusion of deprecated config class

--- src/main/java/com/af/gamerecs/GamerecsApplication.java
+++ src/main/java/com/af/gamerecs/GamerecsApplication.java
@@ -2,12 +2,7 @@ package com.af.gamerecs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.af.gamerecs.config.RawgProperties;
-
-
-@EnableConfigurationProperties(RawgProperties.class)
 @SpringBootApplication
 public class GamerecsApplication {
 
