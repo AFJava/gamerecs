9982dff91b1ae29f81d4afa89adacebea92687d8
AFJava <ayfeng2006@gmail.com>
2026-07-13 22:31:59 -0400

    Remove deprecated files

--- src/main/java/com/af/gamerecs/config/RawgProperties.java
+++
@@ -1,7 +0,0 @@
-package com.af.gamerecs.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-/* Access point for RAWG key and base URL from application.properties */
-@ConfigurationProperties(prefix = "rawg.api")
-public record RawgProperties(String key, String baseUrl) {}
