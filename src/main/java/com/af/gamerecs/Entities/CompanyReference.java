c241218ce1c6528623296785192737b8f06bf8ed
AFJava <ayfeng2006@gmail.com>
2026-07-20 18:59:41 -0400

    Compact reference storage, store date added

--- src/main/java/com/af/gamerecs/Entities/CompanyReference.java
+++ src/main/java/com/af/gamerecs/Entities/CompanyReference.java
@@ -1,5 +1,7 @@
 package com.af.gamerecs.entities;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,7 +19,7 @@ import jakarta.persistence.UniqueConstraint;
     uniqueConstraints = {
         @UniqueConstraint(columnNames = {
             "involved_company_id",
-            "feature_id"
+            "company_id"
         })
     }
 )
@@ -29,23 +31,36 @@ public class CompanyReference {
     @Column(unique = true)
     Long involvedCompanyId;
 
-    @Embedded
-    Feature company;
+    Long companyId;
+
+    FeatureType role;
+
+    LocalDate added;
 
     public CompanyReference() {
-
+        added = LocalDate.now();
     }
 
-    public CompanyReference(Long involvedCompanyId, Feature company) {
+    public CompanyReference(Long involvedCompanyId, Long companyId, FeatureType role) {
         this.involvedCompanyId = involvedCompanyId;
-        this.company = company;
+        this.companyId = companyId;
+        this.role = role;
+        added = LocalDate.now();
     }
 
     public Long getInvolvedCompanyId() {
         return involvedCompanyId;
     }
 
-    public Feature getCompanyId() {
-        return company;
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public FeatureType getRole() {
+        return role;
+    }
+
+    public LocalDate getAdded() {
+        return added;
     }
 }
