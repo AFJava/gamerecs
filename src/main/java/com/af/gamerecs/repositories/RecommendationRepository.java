fd7ebd00d458d6e5b1f0eb36c04e01c6164e9c4d
AFJava <ayfeng2006@gmail.com>
2026-07-22 12:19:45 -0400

    Add recommendation query method

--- src/main/java/com/af/gamerecs/repositories/RecommendationRepository.java
+++ src/main/java/com/af/gamerecs/repositories/RecommendationRepository.java
@@ -1,9 +1,18 @@
 package com.af.gamerecs.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.af.gamerecs.entities.Recommendation;
 
-public interface RecommendationRepository extends JpaRepository<Recommendation, Integer>{
-
+public interface RecommendationRepository extends JpaRepository<Recommendation, Integer> {
+    @Query("""
+        select rec
+        from Recommendation rec
+        where rec.user.id = :userId
+        and rec.game.igdbId in :igdbIds
+    """)
+    public List<Recommendation> findAllMatchingUserRecommendations(Long userId, List<Long> igdbIds);
 }
--- src/main/java/com/af/gamerecs/service/RecommendationService.java
+++ src/main/java/com/af/gamerecs/service/RecommendationService.java
@@ -1,11 +1,21 @@
 package com.af.gamerecs.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.af.gamerecs.entities.Recommendation;
 import com.af.gamerecs.repositories.RecommendationRepository;
 
+@Service
 public class RecommendationService {
     public final RecommendationRepository recommendationRepository;
 
     public RecommendationService(RecommendationRepository recommendationRepository) {
         this.recommendationRepository = recommendationRepository;
+    }
+
+    public List<Recommendation> getExistingRecommendations(Long userId, List<Long> igdbIds) {
+        return recommendationRepository.findAllMatchingUserRecommendations(userId, igdbIds);
     }
 }
