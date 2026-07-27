410f83f72eda8b6f7c579151921ce70b3bc80508
AFJava <ayfeng2006@gmail.com>
2026-07-27 16:17:54 -0400

    Remove profile check

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -17,28 +17,25 @@ function renderAdded(summaryDiv, igdbId) {
     //Check whether to display added game 
     const profileGameDivContainer = document.querySelector(".added-games-container");
 
-    //If on profile page
-    if (profileGameDivContainer !== null) {
-        const profileGameDiv = profileGameDivContainer.querySelector(".added-games");
-        let expandAdded = profileGameDivContainer.querySelector(".added-nav-container");
+    const profileGameDiv = profileGameDivContainer.querySelector(".added-games");
+    let expandAdded = profileGameDivContainer.querySelector(".added-nav-container");
 
-        if (profileGameDiv.childElementCount < 5) {
-            //Use JS to display newly added game without refresh (use Thymeleaf for games previously added)
-            const profileCard = document.createElement("div");
-            profileCard.classList.add("profile-card");
-            profileCard.innerHTML = `<img src = ${imageSrc} class = "game-preview">
-                <h2 class = "game-name">${gameName}</h2>
-                <p class="game-rating">Rating: ${rating} / 10</p>`;
+    if (profileGameDiv.childElementCount < 5) {
+        //Use JS to display newly added game without refresh (use Thymeleaf for games previously added)
+        const profileCard = document.createElement("div");
+        profileCard.classList.add("profile-card");
+        profileCard.innerHTML = `<img src = ${imageSrc} class = "game-preview">
+            <h2 class = "game-name">${gameName}</h2>
+            <p class="game-rating">Rating: ${rating} / 10</p>`;
 
-            profileGameDiv.appendChild(profileCard);
-        } //Otherwise add the new button (if not already rendered)
-        else if (expandAdded === null) {
-            expandAdded = document.createElement("div");
-            expandAdded.classList.add("added-nav-container");
+        profileGameDiv.appendChild(profileCard);
+    } //Otherwise add the new button (if not already rendered)
+    else if (expandAdded === null) {
+        expandAdded = document.createElement("div");
+        expandAdded.classList.add("added-nav-container");
 
-            expandAdded.innerHTML = '<a href="profile/added?page=1" class="button">All added games</a>';
+        expandAdded.innerHTML = '<a href="profile/added?page=1" class="button">All added games</a>';
 
-            profileGameDivContainer.appendChild(expandAdded);
-        }
+        profileGameDivContainer.appendChild(expandAdded);
     }
 }
