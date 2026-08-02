4bf09061df3795128d81554e28606d0751000c33
AFJava <ayfeng2006@gmail.com>
2026-08-02 18:06:48 -0400

    Add deletion endpoint

--- src/main/java/com/af/gamerecs/Controllers/GameController.java
+++ src/main/java/com/af/gamerecs/Controllers/GameController.java
@@ -6,7 +6,9 @@ import java.util.HashSet;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -158,6 +160,18 @@ public class GameController {
         userGameService.saveToProfile(userGame);
         userPreferenceService.updatePreferenceFromGame(user, game, favGameRating);
         
+        return "";
+    }
+
+    @DeleteMapping("/remove/{igdbId}") 
+    public String remove(Authentication authentication, @PathVariable Long igdbId) {
+        Object principal = authentication.getPrincipal();
+        User user = currentUserService.userFromPrincipal(principal);
+
+        Long userId = user.getId();
+
+        userGameService.removeUserGame(userId, igdbId);
+
         return "";
     }
 }
