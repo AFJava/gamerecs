f7387c701401e5c840e6454b9ee6d2a1224d0eef
AFJava <ayfeng2006@gmail.com>
2026-08-09 17:23:05 -0400

    Tune weight multipliers, add age ratings to enum

--- src/main/java/com/af/gamerecs/Entities/FeatureType.java
+++ src/main/java/com/af/gamerecs/Entities/FeatureType.java
@@ -1,6 +1,8 @@
 package com.af.gamerecs.entities;
 
 public enum FeatureType {
+    ESRB_RATING,
+    PEGI_RATING,
     FRANCHISE,
     PUBLISHER,
     DEVELOPER,
@@ -30,19 +32,26 @@ public enum FeatureType {
             || this == FeatureType.PORTING;
     }
 
+    public boolean isAgeRating() {
+        return this == FeatureType.ESRB_RATING
+            || this == FeatureType.PEGI_RATING;
+    }
+
     public double getWeightMultiplier() {
         return switch(this) {
             case FRANCHISE -> 1.0;
-            case PUBLISHER -> 0.5;
-            case DEVELOPER -> 0.8;
-            case SUPPORTING -> 0.3;
+            case PUBLISHER -> 0.4;
+            case DEVELOPER -> 0.7;
+            case SUPPORTING -> 0.2;
             case PORTING -> 0.1;
-            case PLATFORM -> 0.7;
-            case GENRE -> 1.2;
-            case THEME -> 1.2;
-            case GAME_MODE -> 0.8;
+            case PLATFORM -> 0.6;
+            case GENRE -> 1.5;
+            case THEME -> 1.3;
+            case GAME_MODE -> 0.6;
             case PLAYER_PERSPECTIVE -> 0.7;
-            case KEYWORD -> 0.5;
+            case KEYWORD -> 0.6;
+            case ESRB_RATING -> 0.5;
+            case PEGI_RATING -> 0.5;
         };
     }
 }
