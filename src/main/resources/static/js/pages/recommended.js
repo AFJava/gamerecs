d91ce6f87f95a66f9ab360b872363796e3b5ccf2
AFJava <ayfeng2006@gmail.com>
2026-08-12 21:55:01 -0400

    Add intersection observer

--- src/main/resources/static/js/pages/recommended.js
+++ src/main/resources/static/js/pages/recommended.js
@@ -5,6 +5,28 @@ import { rec } from "../service/rec.js"
 const recButtonContainer = document.getElementById("rec-button-container");
 const newRecButton = recButtonContainer.querySelector(".rec-button");
 const recDiv = document.getElementById("rec-games");
+const impressionGameIds = [];
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                const igdbId = entry.target.dataset.igdbId;
+                impressionGameIds.push(igdbId);
+
+                console.log("Game with igdbId " + igdbId + " added to list");
+                observer.unobserve(entry.target);
+            }
+        })
+    },
+    {
+        threshold: 0.5
+    }
+);
+
+recDiv.querySelectorAll(".game-display").forEach((display) => {
+    observer.observe(display);
+});
 
 recDiv.addEventListener("click", (event) => {
     if (event.target.classList.contains("rate-button")) {
